dec5e30b82abc6978049c6e7b99f261baee57619
bakushoda <66668783+bakushoda@users.noreply.github.com>
2021-02-12 22:20:33 +0900

    Update mission_3-4.php

--- mission_3-4.php
+++ mission_3-4.php
@@ -6,8 +6,97 @@
     </head>
     <body>
         
-        <!--投稿フォーム-->
-        <form action="" method="POST">
+<?php
+    $filename = "mission_3-4.txt";
+    
+    // 投稿フォームの変数定義
+    $name = $_POST["name"];
+    $comment = $_POST["comment"];
+    date_default_timezone_set('Asia/Tokyo');
+    $date = date("Y/m/d H:i:s");
+    $editNumber = $_POST["editNumber"];
+    
+    // 削除フォームの変数定義
+    $delete = $_POST["delete"];
+    
+    // 編集フォームの変数定義
+    $edit = $_POST["edit"];
+    
+    //通し番号
+    $fp = fopen($filename, "r");
+    for( $count = 1; fgets( $fp ); $count++ ); 
+    fclose($fp);
+    
+    //ファイルへの書き込み（投稿フォーム）
+    if(isset($name, $comment)  && empty($editNumber)) {
+        $fp = fopen($filename,"a");
+        $data = $count . "<>" . $name . "<>" . $comment . "<>" . $date . PHP_EOL; 
+        fwrite($fp, $data);
+        fclose($fp);
+    } else {
+        if(file_exists($filename)) {
+            $fp = fopen($filename,"a");
+            $items = file($filename, FILE_IGNORE_NEW_LINES);
+            foreach($items as $item){
+                        if (isset($item)) {
+                                $splitItem = explode("<>", $item);
+                                // 投稿番号を取得
+                                $postNumber = $splitItem[0];
+                                if ($postNumber === $editNumber) {
+                                    // 編集したいnameとcommentの値を取得
+                                    $edit2 = $editNumber;
+                                    $editName2 = $name;
+                                    $editComment2 = $comment;
+                                    
+                                    $change_data = $edit2 . "<>" . $editName2 . "<>" . $editComment2 . 
+                                    "<>";
+                                    file_put_contents($filename, $change_data . PHP_EOL);
+                                } 
+        
+    }
+    
+    //ファイルへの書き込み（削除フォーム）
+    if(isset($delete)){ //delete変数があるか判定
+            $items = file($filename, FILE_IGNORE_NEW_LINES);//items変数を定義
+            $fp = fopen($filename, "w");//新規ファイルに書き込み
+            fclose($fp);//ファイルをクローズ
+        foreach($items as $item) {//配列を変数に代入してループ処理
+            $outcome =explode("<>", $item);//文字列を分割
+            if($delete !== $outcome[0]) {//$deleteと$outcome[0]が一致しない場合
+                $fp = fopen($filename,"a");//追記モード
+                fwrite($fp, $item.PHP_EOL);//書き込み
+                fclose($fp);//クローズ
+            }else {
+                //変化なし
+            }
+        }
+    }
+    
+    //ファイルへの書き込み（編集フォーム） 
+    if(isset($edit)) {
+        $fp = fopen($filename,"r"); 
+        $items = file($filename, FILE_IGNORE_NEW_LINES);
+        foreach($items as $item) {
+            $splitItem = explode("<>", $item);
+            $postNumber = $splitItem[0];
+            
+            if($postNumber === $edit) {
+                $editName = $splitItem[1];
+                $editComment = $splitItem[2];
+                
+                fclose($fp);
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    ?>
+    
+    <!--投稿フォーム-->
+        <form action="mission_3-4.php" method="POST">
             <input type="text" name="name" value="<?php echo $editName; ?>" placeholder="名前"><br>
             <input type="text" name="comment" value="<?php echo $editComment; ?>" placeholder="コメント">
             <input type="hidden" name="editNumber" value="<?php echo $editNumber; ?>">
@@ -32,75 +121,10 @@
                 <input type="submit" name="submit" value="編集">
             </form>
         </p>
-        
-        
-<?php
-    $filename = "mission_3-4.txt";
-    
-    // 投稿フォームの変数定義
-    $name = $_POST["name"];
-    $comment = $_POST["comment"];
-    date_default_timezone_set('Asia/Tokyo');
-    $date = date("Y/m/d H:i:s");
-    
-    // 削除フォームの変数定義
-    $delete = $_POST["delete"];
-    
-    // 編集フォームの変数定義
-    $editNumber = $_POST["editNumber"];
-    
-    //通し番号
-    $fp = fopen($filename, "r");
-    for( $count = 1; fgets( $fp ); $count++ ); 
-    fclose($fp);
-    
-    //ファイルへの書き込み（投稿フォーム）
-    if(isset($name, $comment)  && empty($editNumber)) {
-        $fp = fopen($filename,"a");
-        $data = $count . "<>" . $name . "<>" . $comment . "<>" . $date . PHP_EOL; 
-        fwrite($fp, $data);
-        fclose($fp);
-    } 
-    
-    //ファイルへの書き込み（削除フォーム）
-    if(isset($delete)){ //delete変数があるか判定
-            $items = file($filename, FILE_IGNORE_NEW_LINES);//items変数を定義
-            $fp = fopen($filename, "w");//新規ファイルに書き込み
-            fclose($fp);//ファイルをクローズ
-        foreach($items as $item) {//配列を変数に代入してループ処理
-            $outcome =explode("<>", $item);//文字列を分割
-            if($delete !== $outcome[0]) {//$deleteと$outcome[0]が一致しない場合
-                $fp = fopen($filename,"a");//追記モード
-                fwrite($fp, $item.PHP_EOL);//書き込み
-                fclose($fp);//クローズ
-            }else {
-                //変化なし
-            }
-        }
-    }
-    
-    //ファイルへの書き込み（編集フォーム） 
-    if(isset($editNumber)) {
-        $fp = fopen($filename,"r"); 
-        $items = file($filename, FILE_IGNORE_NEW_LINES);
-        foreach($items as $item) {
-            $splitItem = explode("<>", $item);
-            $postNumber = $splitItem[0];
-            
-            if($postNumber === $editNumber) {
-                $editName = $splitItem[1];
-                $editComment = $splitItem[2];
-                
-                fclose($fp);
-            }
-            
-        }
-        
-    }
     
     
         
-    
+    <?php
     //ブラウザへの表示
     if(file_exists($filename)) {
         $lines = file($filename, FILE_IGNORE_NEW_LINES);
